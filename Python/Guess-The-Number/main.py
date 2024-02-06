efb091a1a60e1010fd9fe2fbd1082805284ee100
BusinessCoffee <levi.businesscoffee@gmail.com>
2024-02-05 18:38:20 -0800

    finished number guess

--- Python/Guess-The-Number/main.py
+++ Python/Guess-The-Number/main.py
@@ -1,1 +1,45 @@
 import random
+import subprocess as sub
+
+maxNumber = 100
+lowestNumber = 1
+guesses = 0
+
+def user_choice(text):
+    global guesses
+    try:
+        userChoice = input(text)
+        if userChoice == "/exit":
+            return userChoice
+        else:
+            guesses += 1
+            return int(userChoice)
+    except:
+            print("⚠️  Not a valid entry")  
+
+def setup():
+    botChoice = random.randint(lowestNumber, maxNumber)
+    userChoice = user_choice("Pick a random number between 1-100! -   ")
+    return botChoice, userChoice
+
+def main(botChoice, userChoice):
+    global guesses
+    while 1:  
+        if userChoice == botChoice:
+            print(f"✓   -   {guesses} guesses!")
+            guesses = 0
+            userChoice = user_choice("New game [1: Yes | 0: No] -   ")
+            if userChoice:
+                botChoice = random.randint(lowestNumber, maxNumber)
+                userChoice = user_choice("Pick a random number between 1-100! -   ")
+            else:
+                break
+        elif userChoice > botChoice:
+            userChoice = user_choice("Pick ↓ -   ")
+        elif userChoice < botChoice:
+            userChoice = user_choice("Pick ↑ -   ")
+        elif userChoice == "/exit":
+            break
+
+botChoice, userChoice = setup()
+main(botChoice, userChoice)
